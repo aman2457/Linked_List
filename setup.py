bab5f8dc4b98c903d353c65795b57df770c1db19
Aman <amankumar84349@gmail.com>
2021-04-21 12:12:42 +0530

    final commit v0.0.1

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ def readme():
 
 
 setup(
-    name = "Linked_List",
-    version = "0.1.0",
-    description = "A Python package to work with LinkedList.",
+    name = "py_linked_list",
+    version = "0.0.1",
+    description = "A Python package which contains some methods to manipulate a linked List.",
     long_description = readme(),
     long_description_content_type = "text/markdown",
     url = "https://github.com/aman2457/Linked_List",
     download_url = "https://github.com/aman2457/Linked_List/archive/refs/heads/main.zip",
     author = "Aman Kumar",
-    author_email = "",
+    author_email = "amankumar84349@gmail.com",
     license = "MIT",
     keywords = ["linked list", "list", "python linked list"],
     classifiers = [
@@ -32,4 +32,5 @@ setup(
     package_dir = {'': 'src'},
     include_package_data = True,
     install_requires = [],
+
 )
